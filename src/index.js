196ce30199f3385d65e34d8c94edc319fac52b6c
timDeHof <this.is.tim.dehof@gmail.com>
2022-04-21 15:01:20 -0400

    had code review

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './components';
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./components";
+import AuthProvider from "./components/AuthProvider";
+import { BrowserRouter as Router } from "react-router-dom";
 // css stylesheets can be created for each component
 // place them in the src/style directory, and import them like this:
-import './style/index.css';
+import "./style/index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </Router>,
+  document.getElementById("root")
+);
