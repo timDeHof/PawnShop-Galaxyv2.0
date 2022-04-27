fb35369a49509c6e57bc732b4847d714b17081aa
Aheiy1 <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>
2022-04-27 09:27:14 -0500

    working on creating new cart

--- src/axios-services/cart.js
+++ src/axios-services/cart.js
@@ -2,8 +2,29 @@ import axios from "axios";
 
 export async function getCartByUser(userId) {
   try {
-    const { data: [cart] } = await axios.get(`/api/users/cart/${userId}`);
+    const {
+      data: [cart],
+    } = await axios.get(`/api/users/cart/${userId}`);
 
+    return cart;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function createCart(token, userId, isActive) {
+  try {
+    const {
+      data: [cart],
+    } = await axios.post(`/api/users/cart/${userId}`, {
+      isActive,
+      withCredentials: true,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log(cart, "from axios")
     return cart;
   } catch (err) {
     console.error(err);
--- src/components/CartProvider.jsx
+++ src/components/CartProvider.jsx
@@ -1,31 +1,40 @@
 import React, { useEffect, useState } from "react";
 import CartContext from "../CartContext";
 import useAuth from "../hooks/useAuth";
-import { getCartByUser} from "../axios-services/cart";
+import { getCartByUser, createCart } from "../axios-services/cart";
 import { getProductOrders } from "../axios-services/product-orders";
 import { getProducts } from "../axios-services/products";
 
-
 const CartProvider = ({ children }) => {
   const { token, user } = useAuth();
-  const [cart, setCart] = useState({product_orders: []});
-//   const localCart = JSON.parse(localStorage.getItem("cart"));
-//   console.log(localCart, "my local cart");
-//   console.log(cart, "This cart");
-//   if (localCart) {
-//     console.log("localCart exists");
-//   } else {
-//     console.log("localCart is not found");
-//   }
+  const [cart, setCart] = useState({ product_orders: [] });
+  console.log(cart, "before use effect")
+  //   const localCart = JSON.parse(localStorage.getItem("cart"));
+  //   console.log(localCart, "my local cart");
+  //   console.log(cart, "This cart");
+  //   if (localCart) {
+  //     console.log("localCart exists");
+  //   } else {
+  //     console.log("localCart is not found");
+  //   }
   useEffect(() => {
-    if (user.id) {
+    console.log(cart, "in useeffect");
+    if (user.id && cart.id) {
       async function getCart() {
-        const userCart = await getCartByUser(user.id)
-        console.log(userCart)
-        setCart(userCart)
+        const userCart = await getCartByUser(user.id);
+        // console.log(userCart);
+        setCart(userCart);
       }
 
       getCart();
+    }
+    if (user.id && !cart.id) {
+      async function newCart() {
+        const newUserCart = await createCart(user.id);
+        // console.log(newUserCart, "new user cart");
+        setCart(newUserCart);
+      }
+      newCart();
     }
   }, [user]);
 
