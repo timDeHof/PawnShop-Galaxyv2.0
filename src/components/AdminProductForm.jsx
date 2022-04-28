8828f132f1d2b580d464f366de1f50837a9ceac8
Jacob <your_email@real_email.edu>
2022-04-28 00:07:54 -0500

    added funny log gifs lol

--- src/components/AdminProductForm.jsx
+++ src/components/AdminProductForm.jsx
@@ -26,14 +26,10 @@ const AdminProductForm = () => {
                 !!formState.condition,
                 formState.imageURL
               );
-              console.log(
-                "%cCreated New Product",
-                `background:linear-gradient(#E66465, #9198E5);padding: .3rem;color: white;border-radius: .5em`
+              console.log("%cNew Product Created", `background:linear-gradient(#E66465, #9198E5);padding: .3rem;color: white;border-radius: .5em`
               );
-              console.log(
-                "%c ",
-                "font-size: 1px; padding: 125px 125px; background-size: 250px 250px; background: no-repeat url(https://i2.wp.com/i.giphy.com/media/12BYUePgtn7sis/giphy-downsized.gif?w=770&amp;ssl=1);"
-              );
+              console.log("%c ", "font-size: 1px; padding: 125px 125px; background-size: 250px 250px; background: no-repeat url(https://i2.wp.com/i.giphy.com/media/12BYUePgtn7sis/giphy-downsized.gif?w=770&amp;ssl=1);"
+              ); 
             }}
           >
             <div className={styles.user_box}>
