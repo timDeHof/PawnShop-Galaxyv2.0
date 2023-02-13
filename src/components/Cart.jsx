71c8ec0417876cb7d62f89198719677ca9c8c027
timDeHof <this.is.tim.dehof@gmail.com>
2023-02-13 17:36:55 -0500

    fix: disables buttons while async call is processing

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect,useRef } from "react"
 import useAuth from "../hooks/useAuth"
 import useCart from "../hooks/useCart"
 import { removeFromCart } from "../axios-services/product-orders"
@@ -7,13 +7,23 @@ import { useNavigate } from "react-router-dom"
 const Cart = () => {
   const { token, user } = useAuth()
   const { cart, setCart, updateQty, deleteItem, checkout } = useCart()
-  const [qty, setQty] = useState(1)
-  const incrementQty = () => setQty(qty + 1)
-  let decrementQty = () => setQty(qty - 1)
+  const [loading, setLoading] = useState(false);
+  // const [qty, setQty] = useState(1)
+  // const incrementQty = () => setQty(qty + 1)
+  // let decrementQty = () => setQty(qty - 1)
 
   const navigate = useNavigate()
-
+  const quantityButton = useRef();
   console.log("The Cart:", cart)
+
+  useEffect(() => {
+    if (loading) {
+      quantityButton.current.disabled = true;
+    } else {
+      quantityButton.current.removeAttribute("disabled");
+    }
+  }, [loading])
+
   return (
     <div>
       <br />
@@ -36,23 +46,27 @@ const Cart = () => {
 
       {cart.product_orders.map((product_order, i) => {
         return (
-          <div key={`cartproduct${i}`}>
+          <div key={`cartProduct${i}`}>
             <h2>{product_order.products.name}</h2>
             <img src={product_order.products.imageURL} width='200px' />
             <div>
               $ {parseFloat(product_order.products.price * product_order.quantity).toFixed(2)}
             </div>
             <button
+              ref={quantityButton}
+              disabled={ product_order.quantity === 1 ? true : false}
               onClick={async () => {
-                updateQty(product_order.id, --product_order.quantity)
-              }}
-              disabled={product_order.quantity === 1 ? true : false}>
+                setLoading(true)
+                updateQty(product_order.id, --product_order.quantity).then(() => setLoading(false))
+              }}>
               -
             </button>
             <div>{product_order.quantity}</div>
             <button
+              ref={quantityButton}
               onClick={async () => {
-                updateQty(product_order.id, ++product_order.quantity)
+                setLoading(true)
+                updateQty(product_order.id, ++product_order.quantity).then(() => setLoading(false))
               }}>
               +
             </button>
