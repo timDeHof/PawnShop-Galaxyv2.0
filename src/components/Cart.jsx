cce4821a06616a71111a397a71d3b8842ec1b416
timDeHof <this.is.tim.dehof@gmail.com>
2023-02-13 15:13:03 -0500

    fix: formatted cart pricing to a number

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,19 +1,19 @@
-import React, { useState, useEffect } from "react";
-import useAuth from "../hooks/useAuth";
-import useCart from "../hooks/useCart";
-import { removeFromCart } from "../axios-services/product-orders";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react"
+import useAuth from "../hooks/useAuth"
+import useCart from "../hooks/useCart"
+import { removeFromCart } from "../axios-services/product-orders"
+import { useNavigate } from "react-router-dom"
 
 const Cart = () => {
-  const { token, user } = useAuth();
-  const { cart, setCart, updateQty, deleteItem, checkout } = useCart();
-  const [qty, setQty] = useState(1);
-  const incrementQty = () => setQty(qty + 1);
-  let decrementQty = () => setQty(qty - 1);
+  const { token, user } = useAuth()
+  const { cart, setCart, updateQty, deleteItem, checkout } = useCart()
+  const [qty, setQty] = useState(1)
+  const incrementQty = () => setQty(qty + 1)
+  let decrementQty = () => setQty(qty - 1)
 
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
-  console.log("The Cart:", cart);
+  console.log("The Cart:", cart)
   return (
     <div>
       <br />
@@ -25,13 +25,12 @@ const Cart = () => {
 
       <button
         onClick={async () => {
-          const orderId = cart.id;
-          console.log("OrderId", orderId);
-          await checkout(orderId, user.id);
+          const orderId = cart.id
+          console.log("OrderId", orderId)
+          await checkout(orderId, user.id)
 
           // navigate("/", { replace: true });
-        }}
-      >
+        }}>
         Checkout
       </button>
 
@@ -39,37 +38,36 @@ const Cart = () => {
         return (
           <div key={`cartproduct${i}`}>
             <h2>{product_order.products.name}</h2>
-            <img src={product_order.products.imageURL} width="200px" />
-            <div>$ {(product_order.products.price * product_order.quantity).toFixed(2)}</div>
+            <img src={product_order.products.imageURL} width='200px' />
+            <div>
+              $ {parseFloat(product_order.products.price * product_order.quantity).toFixed(2)}
+            </div>
             <button
               onClick={async () => {
-                updateQty(product_order.id, --product_order.quantity);
-              }}
-            >
+                updateQty(product_order.id, --product_order.quantity)
+              }}>
               -
             </button>
             <div>{product_order.quantity}</div>
             <button
               onClick={async () => {
-                updateQty(product_order.id, ++product_order.quantity);
-              }}
-            >
+                updateQty(product_order.id, ++product_order.quantity)
+              }}>
               +
             </button>
             <div>
               <button
                 onClick={async () => {
-                  deleteItem(token, product_order.id);
-                }}
-              >
+                  deleteItem(token, product_order.id)
+                }}>
                 Remove Item
               </button>
             </div>
           </div>
-        );
+        )
       })}
     </div>
-  );
-};
+  )
+}
 
-export default Cart;
+export default Cart
