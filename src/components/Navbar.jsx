ca12eeea698bb7c0d15ce2c3bf42691f9af20b09
timDeHof <this.is.tim.dehof@gmail.com>
2023-04-19 20:20:55 -0400

    Update CI configuration, add .prettierignore and .prettierrc files, update font styling, and update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,59 +11,59 @@ function Navbar() {
 
   return (
     <ul>
+      <li>
+        <NavLink activeclassname="active" to="/">
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink activeclassname="active" to="/products">
+          Products
+        </NavLink>
+      </li>
+      {token ? (
         <li>
-          <NavLink activeclassname="active" to="/">
-            Home
+          <NavLink
+            activeclassname="active"
+            to="/login"
+            onClick={() => {
+              localStorage.removeItem('token');
+              setToken(localStorage.getItem('token'));
+              setUser({});
+            }}
+          >
+            Logout
           </NavLink>
         </li>
-        <li>
-          <NavLink activeclassname="active" to="/products">
-            Products
-          </NavLink>
-        </li>
-        {token ? (
+      ) : (
+        <>
           <li>
-            <NavLink
-              activeclassname="active"
-              to="/login"
-              onClick={() => {
-                localStorage.removeItem('token');
-                setToken(localStorage.getItem('token'));
-                setUser({});
-              }}
-            >
-              Logout
+            <NavLink activeclassname="active" to="/login">
+              Log In
             </NavLink>
           </li>
-        ) : (
-          <>
-            <li>
-              <NavLink activeclassname="active" to="/login">
-                Log In
-              </NavLink>
-            </li>
-            <li>
-              <NavLink activeclassname="active" to="/register">
-                Register
-              </NavLink>
-            </li>
-          </>
-        )}
+          <li>
+            <NavLink activeclassname="active" to="/register">
+              Register
+            </NavLink>
+          </li>
+        </>
+      )}
 
-        {!user.isAdmin ? (
-          <li>
-            <NavLink activeclassname="active" to="/cart">
-              <FontAwesomeIcon icon={faShoppingCart} />
-            </NavLink>
-          </li>
-        ) : (
-          <li>
-            <NavLink activeclassname="active" to="/admin/dashboard">
-              Dashboard
-            </NavLink>
-          </li>
-        )}
-      </ul>
+      {!user.isAdmin ? (
+        <li>
+          <NavLink activeclassname="active" to="/cart">
+            <FontAwesomeIcon icon={faShoppingCart} />
+          </NavLink>
+        </li>
+      ) : (
+        <li>
+          <NavLink activeclassname="active" to="/admin/dashboard">
+            Dashboard
+          </NavLink>
+        </li>
+      )}
+    </ul>
   );
 }
 
